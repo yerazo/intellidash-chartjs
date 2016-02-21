4984c95e8e12617a0b39ffe4cad4cfa9f3090b76
Yara Erazo <yaraerazo@gmail.com>
2016-02-20 22:11:32 -0600

    attempt to add a full table row of information results in one column

--- assets/js/customers.js
+++ assets/js/customers.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
-	$.get('https://www.batchacademy.com/api/wdfne/dummy/intellidash', function(response){
+	$.get('https://www.batchacademy.com/api/wdfne/dummy/intellidash', function(data){
 		for(var i = 0; i < data.length; i++){
+			console.log(data[i]);
 			var newTableRow = $('<tr/>');
+			var newTableData = $('<td/>');
+			$(newTableRow).append(newTableData);
+
+			$(newTableData, newTableRow).text(data[i].id);
+			// $('<td/>', newTableRow).text(data[i].person);
+			$(newTableData, newTableRow).text(data[i].person.email);
+			$(newTableData, newTableRow).text(data[i].person.location.city);
+			$(newTableData, newTableRow).text(data[i].person.location.state);
+			$(newTableData, newTableRow).text(data[i].person.race);
+			$(newTableData, newTableRow).text(data[i].person.gender);
+
+			$('tbody').append(newTableRow);
 
 		}
 		// fixSidebar();
